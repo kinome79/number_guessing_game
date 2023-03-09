0d0540ebc1374df3f1170d780ac6e946a63e9f82
kinome79 <kinome79@yahoo.com>
2023-03-08 22:45:18 -0500

    feat: Initial username validation edit

--- number_guess.sh
+++ number_guess.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 PSQL="psql --username=freecodecamp ==dbname=number_guess -t --no-align -c"
+=======
+
+>>>>>>> feat:Initial username validation edit
 
 #Get and validate username
 read -p "Enter your username: " USERNAME
 
 if [[ -z $USERNAME ]]
 then
+  echo "You must enter a username to play!"
+elif [[ ${#USERNAME} > 22 ]]
+then
+  echo "The username supplied is too long (max 22 chars)!"
 else
+
 fi
 
 #Generate and echo random number
