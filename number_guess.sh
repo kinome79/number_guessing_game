03cb5f8977b243510b0268601fbc3f77c6f708de
kinome79 <kinome79@yahoo.com>
2023-03-08 23:56:47 -0500

    test: Test/fix Username Validation

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp ==dbname=number_guess -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 
 #Get and validate username
@@ -10,17 +10,22 @@ if [[ -z $USERNAME ]]
 then
   echo "You must enter a username to play!"
   exit
-elif [[ ${#USERNAME} > 22 ]]
+elif [[ ${#USERNAME} -gt 22 ]]
 then
-  echo "The username supplied is too long (max 22 chars)!"
+  echo "The username supplied is too long at ${#USERNAME} chars (max 22 chars)!"
   exit
 else
   USER_INFO=$($PSQL "SELECT user_id, games_played, best_game FROM users WHERE username='$USERNAME'")
   if [[ -z $USER_INFO ]]
   then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
+    RESPONSE=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
+    if [[ $RESPONSE != "INSERT 0 1" ]]
+    then
+      echo "An error occurred while storing your username!"
+    fi
   else
-    IFS="|" read ID GAMES BEST <<< echo "$USER_INFO"
+    IFS="|" read ID GAMES BEST <<< "$USER_INFO"
     echo "Welcome back, $USERNAME! You have played $GAMES games, and your best game took $BEST guesses."
   fi
 fi
