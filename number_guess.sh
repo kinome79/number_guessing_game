7e8138df77df9d44ae3d5545954f6d9d58f316e7
kinome79 <kinome79@yahoo.com>
2023-03-09 00:29:11 -0500

    feat: Completed game loop code

--- number_guess.sh
+++ number_guess.sh
@@ -33,4 +33,39 @@ fi
 #Generate and echo random number
 RANDOM_NUM=$(($RANDOM%1000 + 1))
 
+VALIDATE_GUESS() {
+  while [[ ! $GUESS =~ ^[0-9]+$ ]]
+  do
+    read -p "That is not an integer, guess again: " GUESS
+  done
+}
+
 #Guessing Loop
+NUM_GUESSES=1
+read -p "Guess the secret number between 1 and 1000: " GUESS
+VALIDATE_GUESS
+while [[ $RANDOM_NUM -ne $GUESS ]]
+do
+  if [[ $RANDOM_NUM -gt $GUESS ]]
+  then
+    read -p "It's higher than that, guess again: " GUESS
+  else
+    read -p "It's lower than that, guess again: " GUESS
+  fi
+  VALIDATE_GUESS
+  (( NUM_GUESSES++ ))
+done
+
+(( GAMES++ ))
+if [[ $BEST > $NUM_GUESSES ]] 
+then
+  BEST=$NUM_GUESSES
+fi
+
+RESPONSE=$($PSQL "UPDATE users SET games_played=$GAMES, best_game=$BEST WHERE username='$USERNAME'")
+if [[ $RESPONSE != "UPDATE 1" ]]
+then
+  echo "An error occurred while storing your game data!"
+fi
+
+echo "You guessed it in $NUM_GUESSES tries. The secret number was $RANDOM_NUM. Nice job!
